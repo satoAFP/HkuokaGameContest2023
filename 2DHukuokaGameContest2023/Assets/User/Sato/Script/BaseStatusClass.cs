e5598b37239014c4b42d9ca60a4c442420a64fd5
DEV3-6\GA3 <h120201907@hiratagakuen.onmicrosoft.com>
2022-10-04 11:04:45 +0900

    ベースクラス作成

---
+++ 2DHukuokaGameContest2023/Assets/User/Sato/Script/BaseStatusClass.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStatusClass : MonoBehaviour
+{
+    private float HP;
+    private float ATK;
+    private float DEF;
+
+
+}
