9deaa3eb34967f72199282f11c7d17ec3bf7bf28
DEV3-6\GA3 <h120201907@hiratagakuen.onmicrosoft.com>
2022-10-04 16:33:20 +0900

    敵の動きも主人公を参考に変更

--- 2DHukuokaGameContest2023/Assets/User/Sato/Script/BaseEnemy.cs
+++ 2DHukuokaGameContest2023/Assets/User/Sato/Script/BaseEnemy.cs
@@ -7,6 +7,9 @@ public class BaseEnemy : BaseStatusClass
     [SerializeField, Header("移動速度"), Range(0, 100)]
     private float MoveSpeed;
 
+    [SerializeField, Header("最高速度"), Range(0, 20)]
+    private float LimitSpeed;
+
     [SerializeField, Header("移動時間"), Range(0, 1000)]
     private float MoveFrame;
 
@@ -20,11 +23,12 @@ public class BaseEnemy : BaseStatusClass
     private float raydistance;
 
 
-    private Rigidbody2D rigidbody;
-    private int MoveCount = 0;
+    private Rigidbody2D rigidbody;              //リジットボディ2D取得
+    private int MoveCount = 0;                  //左右移動切り替えのタイミング取得用
     private Vector2 RayRotato;                  //レイの回転位置決定変数
     private float rotato = 0;                   //回転量
     private GameObject SearchGameObject;        //レイに触れたオブジェクト
+    private bool AttckMode = false;             //主人公を見つけた時の攻撃モード
 
     // Start is called before the first frame update
     void Start()
@@ -35,26 +39,13 @@ public class BaseEnemy : BaseStatusClass
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        //重力設定
-        //Physics2D.gravity = new Vector3(0, -Gravity, 0);
-
-        //移動処理
-        MoveCount++;
-        if (MoveCount <= (MoveFrame / 2)) 
-        {
-            rigidbody.velocity = new Vector3(-MoveSpeed, -5, 0);
-        }
-        if (MoveCount > (MoveFrame / 2) && MoveCount <= MoveFrame) 
-        {
-            rigidbody.velocity = new Vector3(MoveSpeed, -5, 0);
-        }
-        if (MoveCount > MoveFrame) 
-        {
-            MoveCount = 0;
-        }
+        //重力設定-----------------------------------------------------------------------------------------------------------------
+        Physics2D.gravity = new Vector3(0, -Gravity, 0);
 
 
+
+
+        //主人公サーチ-------------------------------------------------------------------------------------------------------------
         //オブジェクトから右側にRayを伸ばす
         Ray2D ray = new Ray2D(transform.position, transform.right);
         SearchGameObject = null;
@@ -76,8 +67,52 @@ public class BaseEnemy : BaseStatusClass
 
             if (SearchGameObject.tag == "Player")
             {
-                
+                AttckMode = true;
             }
         }
+
+
+
+        //移動処理----------------------------------------------------------------------------------------------------------------
+        //攻撃モードに入っていないときの行動
+        if (!AttckMode)
+        {
+            MoveCount++;
+            if (MoveCount <= (MoveFrame / 2))
+            {
+                //最高速度になるとそれ以上加速しない
+                if (rigidbody.velocity.x > -LimitSpeed)
+                {
+                    rigidbody.AddForce(transform.right * (-MoveSpeed), ForceMode2D.Force);
+                }
+            }
+            if (MoveCount > (MoveFrame / 2) && MoveCount <= MoveFrame)
+            {
+                //最高速度になるとそれ以上加速しない
+                if (rigidbody.velocity.x < LimitSpeed)
+                {
+                    rigidbody.AddForce(transform.right * (MoveSpeed), ForceMode2D.Force);
+                }
+            }
+            if (MoveCount > MoveFrame)
+            {
+                //カウントリセット
+                MoveCount = 0;
+            }
+        }
+        //攻撃モードの行動
+        else
+        {
+            //ここ書いてね
+        }
+
+
+
+
+        
+        
+
+        
+
     }
 }
