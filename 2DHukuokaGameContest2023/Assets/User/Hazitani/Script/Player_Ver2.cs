e385946c742eebe05baa96e893d065297461e528
h1202019038 <h1202019038@hiratagakurn.onmicrosoft.com>
2022-10-04 16:33:08 +0900

    プレイヤー移動

--- 2DHukuokaGameContest2023/Assets/User/Hazitani/Script/Player_Ver2.cs
+++ 2DHukuokaGameContest2023/Assets/User/Hazitani/Script/Player_Ver2.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Player_Ver2 : MonoBehaviour
 {
-	[SerializeField, Header("ジャンプ力"), Range(0, 1000)]
+	[SerializeField, Header("ジャンプ力"), Range(0, 100)]
 	private float JumpPower;
 
-	[SerializeField, Header("移動速度"), Range(0, 100)]
+	[SerializeField, Header("移動速度"), Range(0, 200)]
 	private float MoveSpeed;
+
+	[SerializeField, Header("最高速度"), Range(0, 20)]
+	private float LimitSpeed;
 
 	[SerializeField, Header("重力"), Range(0, 100)]
 	private float Gravity;
@@ -31,14 +34,23 @@ public class Player_Ver2 : MonoBehaviour
 		//移動処理
 		if (Input.GetKey(KeyCode.A))
 		{
-			//rigidbody.AddForce(new Vector3(-MoveSpeed, 0, 0));
-			rigidbody.velocity = new Vector3(-MoveSpeed, 0, 0);
+			//最高速度になるとそれ以上加速しない
+			if (rigidbody.velocity.x > -LimitSpeed)
+			{
+				rigidbody.AddForce(transform.right * (-MoveSpeed), ForceMode2D.Force);
+				//rigidbody.velocity = new Vector3(-MoveSpeed, 0, 0);
+			}
 		}
 		if (Input.GetKey(KeyCode.D))
 		{
-			//rigidbody.AddForce(new Vector3(MoveSpeed, 0, 0));
-			rigidbody.velocity = new Vector3(MoveSpeed, 0, 0);
+			//最高速度になるとそれ以上加速しない
+			if (rigidbody.velocity.x < LimitSpeed)
+			{
+				rigidbody.AddForce(transform.right * (MoveSpeed), ForceMode2D.Force);
+				//rigidbody.velocity = new Vector3(MoveSpeed, 0, 0);
+			}
 		}
+
 
 
 		//ジャンプ処理
