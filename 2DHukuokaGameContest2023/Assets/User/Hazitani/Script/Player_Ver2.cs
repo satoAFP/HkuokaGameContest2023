b8e5869380d19e95ffe75807d421fbb9a37c0434
はず家 <h1202019038@hiratagakuen.onmicrosoft.com>
2022-10-06 16:36:36 +0900

    着地判定Reyにするわ

--- 2DHukuokaGameContest2023/Assets/User/Hazitani/Script/Player_Ver2.cs
+++ 2DHukuokaGameContest2023/Assets/User/Hazitani/Script/Player_Ver2.cs
@@ -16,17 +16,92 @@ public class Player_Ver2 : MonoBehaviour
 	[SerializeField, Header("重力"), Range(0, 100)]
 	private float Gravity;
 
+	public enum Direction
+	{
+		LEFT = -1,
+		STOP,
+		RIGHT,
+	}
 
 	private Rigidbody2D rb2D;
-	private bool first = true;
 	private int jump_count = 0;
+	private int now_move = 0;//左:-1・停止:0・右:1
+
+	private Ray2D ray;				//飛ばすレイ
+	private float distance = 0.1f;	//レイを飛ばす距離
+	private RaycastHit2D hit;		//レイが何かに当たった時の情報
+	private Vector3 rayPosition;	//レイを発射する位置
 
 	void Start()
 	{
 		rb2D = GetComponent<Rigidbody2D>();
 	}
 
-	void FixedUpdate()
+	void Update()
+    {
+		//レイを発射する位置の調整
+		rayPosition = transform.position + new Vector3(0.0f, -0.5f, 0.0f);
+		//レイを下に飛ばす
+		ray = new Ray2D(rayPosition, transform.up * -1);
+		//レイを赤色で表示させる
+		Debug.DrawRay(ray.origin, ray.direction * distance, Color.red);
+
+		////レイが当たったとき
+		//if (Physics2D.Raycast())
+		//{
+		//	if (hit.collider.tag == "Ground")
+		//	{
+		//		Debug.Log("レイ当たってます");
+		//		jump_count = 0;
+		//	}
+		//}
+
+		//ジャンプ処理
+		if (Input.GetKeyDown(KeyCode.Space) && jump_count < 2)
+		{
+			Debug.Log("ジャンプ入力された");
+			rb2D.velocity = new Vector2(rb2D.velocity.x, JumpPower);
+
+			//カウント増加
+			jump_count++;
+		}
+
+		//弱攻撃
+		if (Input.GetMouseButtonDown(0))
+		{
+			switch(now_move)
+            {
+				case (int)Direction.LEFT:
+					Debug.Log("左弱キック！");
+					break;
+				case (int)Direction.STOP:
+					Debug.Log("弱パンチ！");
+					break;
+				case (int)Direction.RIGHT:
+					Debug.Log("右弱キック！");
+					break;
+			}
+		}
+
+		//強攻撃
+		if (Input.GetMouseButtonDown(1))
+		{
+			switch (now_move)
+			{
+				case (int)Direction.LEFT:
+					Debug.Log("左強キック！");
+					break;
+				case (int)Direction.STOP:
+					Debug.Log("強パンチ！");
+					break;
+				case (int)Direction.RIGHT:
+					Debug.Log("右強キック！");
+					break;
+			}
+		}
+	}
+
+    void FixedUpdate()
 	{
 		//重力設定
 		Physics2D.gravity = new Vector3(0, -Gravity, 0);
@@ -34,50 +109,34 @@ public class Player_Ver2 : MonoBehaviour
 		//移動処理
 		if (Input.GetKey(KeyCode.A))
 		{
+			now_move = (int)Direction.LEFT;
 			//最高速度になるとそれ以上加速しない
 			if (rb2D.velocity.x > -LimitSpeed)
 			{
 				rb2D.AddForce(-transform.right * (MoveSpeed), ForceMode2D.Force);
-				//rigidbody.velocity = new Vector3(-MoveSpeed, 0, 0);
 			}
 		}
 		if (Input.GetKey(KeyCode.D))
 		{
+			now_move = (int)Direction.RIGHT;
 			//最高速度になるとそれ以上加速しない
 			if (rb2D.velocity.x < LimitSpeed)
 			{
 				rb2D.AddForce(transform.right * (MoveSpeed), ForceMode2D.Force);
-				//rigidbody.velocity = new Vector3(MoveSpeed, 0, 0);
 			}
 		}
-
-
-		//ジャンプ処理
-		if (Input.GetKey(KeyCode.Space) && jump_count < 2)
-		{
-			//1回のみ
-			if (first)
-			{
-				Debug.Log("ジャンプ入力された");
-				rb2D.velocity = new Vector2(rb2D.velocity.x, JumpPower);
-
-				//カウント増加
-				jump_count++;
-				first = false;
-			}
+		if(!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+			now_move = (int)Direction.STOP;
 		}
-		else
-			first = true;
 	}
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-		//Groundタグに触れた場合
-        if(other.gameObject.CompareTag("Ground"))
-        {
-			Debug.Log("着地しました");
-			//カウントリセット
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Ground"))
+		{
+			Debug.Log("着地した！");
 			jump_count = 0;
-        }
-    }
+		}
+	}
 }
