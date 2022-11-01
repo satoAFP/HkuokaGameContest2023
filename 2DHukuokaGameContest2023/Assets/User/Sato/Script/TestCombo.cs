32898784793b5e94e8de19747373294a4e7bca9c
DEV3-6\GA3 <h120201907@hiratagakuen.onmicrosoft.com>
2022-11-01 16:42:09 +0900

    コンボできる

--- 2DHukuokaGameContest2023/Assets/User/Sato/Script/TestCombo.cs
+++ 2DHukuokaGameContest2023/Assets/User/Sato/Script/TestCombo.cs
@@ -14,27 +14,36 @@ public class TestCombo : MonoBehaviour
     [SerializeField, Header("コンボ表示用")]
     public Animator sizeUpAni;
 
+    [SerializeField, Header("コンボ表示用")]
+    public GameObject textMeshObj;
+
     private int ComboNum = 0;
+    private AnimatorStateInfo animStateInfo;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        textMeshObj.gameObject.GetComponent<MeshRenderer>().sortingOrder = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Combo)
+
+
+
+
+
+
+        if (Combo)
         {
             ComboNum++;
-            if (!sizeUpAni.GetBool("SizeUp"))
-                sizeUpAni.SetBool("SizeUp", true);
-
+            sizeUpAni.SetTrigger("SizeUp");
             
 
             Combo = false;
         }
         text.text = "" + ComboNum;
+        textMeshObj.GetComponent<TextMesh>().text = "" + ComboNum;
     }
 }
