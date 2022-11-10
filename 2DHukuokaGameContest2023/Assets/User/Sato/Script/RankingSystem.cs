9193503cdcb24d8c1f82b5f3bfdb44459816c2d4
satoAFP <72423443+satoAFP@users.noreply.github.com>
2022-11-10 16:19:31 +0900

    ソート処理作成

---
+++ 2DHukuokaGameContest2023/Assets/User/Sato/Script/RankingSystem.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankingSystem : MonoBehaviour
+{
+    private int[] Score = new int[10];
+
+    private bool first = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+            Score[i] = Random.Range(0, 100);
+
+        Debug.Log(Score[0] + ":" + Score[1] + ":" + Score[2] + ":" + Score[3] + ":" + Score[4]
+            + ":" + Score[5] + ":" + Score[6] + ":" + Score[7] + ":" + Score[8] + ":" + Score[9]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Sort();
+
+        if (first)
+        {
+            for (int i = 0; i < Score.Length; i++)
+                Debug.Log(Score[i]);
+            first = false;
+        }
+    }
+
+
+    private void Sort()
+    {
+        int max = 0;
+        int max_pos = 0;
+
+        for (int i = 0; i < Score.Length; i++)
+        {
+            max = Score[i];
+            max_pos = i;
+            for (int j = i + 1; j < Score.Length; j++)
+            {
+                if (Score[i] < Score[j])
+                {
+                    if (max < Score[j])
+                    {
+                        max = Score[j];
+                        max_pos = j;
+                    }
+                }
+            }
+            Score[max_pos] = Score[i];
+            Score[i] = max;
+        }
+    }
+
+}
