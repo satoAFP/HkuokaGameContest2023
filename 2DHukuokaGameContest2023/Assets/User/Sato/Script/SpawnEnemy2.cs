78917088934ed6930f42c4fd9669002fb87784fd
DEV3-6\GA3 <h120201907@hiratagakuen.onmicrosoft.com>
2022-11-22 15:36:16 +0900

    敵出現処理変更中、マネージャーアクセッサー作成中

---
+++ 2DHukuokaGameContest2023/Assets/User/Sato/Script/SpawnEnemy2.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy2 : MonoBehaviour
+{
+    [SerializeField, Header("リスポーンまでのフレーム")]
+    private int RespownFrame;
+
+    [SerializeField, Header("出現させるオブジェクト")]
+    private GameObject SpownEnemy;
+
+
+    private int FrameCount = 0; //フレームカウント用
+    private GameObject clone;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (clone == null)
+        {
+            if (RespownFrame <= FrameCount)
+            {
+                clone = Instantiate(SpownEnemy, transform.position, Quaternion.identity);
+                clone.transform.parent = transform;
+                FrameCount = 0;
+            }
+            FrameCount++;
+        }
+
+    }
+}
