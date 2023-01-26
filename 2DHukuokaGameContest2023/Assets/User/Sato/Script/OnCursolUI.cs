4c0f90d861c22b14a5748e72c59e0258fd70a0a8
satoAFP <72423443+satoAFP@users.noreply.github.com>
2023-01-26 16:32:55 +0900

    カーソル合わせるやつ途中

--- 2DHukuokaGameContest2023/Assets/User/Sato/Script/OnCursolUI.cs
+++ 2DHukuokaGameContest2023/Assets/User/Sato/Script/OnCursolUI.cs
@@ -2,20 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 public class OnCursolUI : MonoBehaviour
 {
     [SerializeField, Header("変化させる色")] private Color color;
 
+
     // Update is called once per frame
     void Update()
     {
-        //ボタン上にカーソルがあるとき枠をつける
-        if(EventSystem.current.IsPointerOverGameObject())
-            gameObject.GetComponent<Outline>().effectColor = color;
-        else
-            gameObject.GetComponent<Outline>().effectColor = new Color(1, 1, 1, 0);
+        Vector2 mouse = Input.mousePosition;
+        mouse.x -= 1920 / 2;
+        mouse.y -= 1080 / 2;
 
+        Vector2 pos = gameObject.transform.parent.GetComponent<RectTransform>().localPosition;
+        Vector2 size = gameObject.transform.parent.GetComponent<RectTransform>().sizeDelta;
+
+
+        if (mouse.x > pos.x - (size.x / 2) && mouse.x < pos.x + (size.x / 2) &&
+            mouse.y > pos.y - (size.y / 2) && mouse.y < pos.y + (size.y / 2)) 
+        {
+            gameObject.transform.parent.gameObject.GetComponent<Outline>().effectColor = color;
+            
+        }
+        else
+        {
+            gameObject.transform.parent.gameObject.GetComponent<Outline>().effectColor = new Color(1, 1, 1, 0);
+        }
     }
 }
